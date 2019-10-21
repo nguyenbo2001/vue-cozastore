ae23d7845e7bdc3d8eb83c8b2d618abe4b5e76e8
nguyenbo2001 <nguyenbo2001@gmail.com>
2019-10-21 22:35:40 +0700

    CSS & Animation

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,16 +1,48 @@
 import Vue from 'vue';
-import Vuetify, { VApp, VContent } from 'vuetify/lib';
+import Vuetify, { VApp, VContent, VCol, VIcon, VRow, VCard, VSlider, VHover } from 'vuetify/lib';
 import 'vuetify/dist/vuetify.min.css';
+import 'font-awesome/css/font-awesome.min.css'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify, {
   components: {
     VApp,
     VContent,
+    VCol,
+    VIcon,
+    VRow,
+    VCard,
+    VSlider,
+    VHover,
   }
 });
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
+    iconfont: 'mdi || fa || fa4',
   },
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        // primary: colors.purple,
+        // secondary: colors.grey.darken1,
+        // accent: colors.shades.black,
+        error: colors.red.accent3,
+        primary: colors.red.darken1, // #E53935
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.indigo.base, // #3F51B5
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+      options: {
+        minifyTheme: function (css) {
+          return process.env.NODE_ENV === 'production'
+            ? css.replace(/[\r\n|\r|\n]/g, '')
+            : css
+        },
+      },
+    }
+  }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <div class="purple darken-2 text-center">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, hic?
+      <span class="white--text">Lorem, ipsum.</span>
+    </div>
+    <div class="red">Lorem ipsum dolor sit amet consectetur.</div>
+    <p class="red--text darken-6">Lorem ipsum dolor sit.</p>
+    <p class="indigo--text text-darken-2">indigo darken 2</p>
+    <p class="pink--text accent-4">Lorem ipsum dolor sit.</p>
+    <blockquote class="blockquote">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci sunt voluptate nesciunt a pariatur ipsa sed accusamus aliquam consequuntur corrupti omnis iure quidem illum possimus, iste ullam modi, incidunt necessitatibus?</blockquote>
+    <var>v</var> = <var>u</var> * <var>e</var>
+
+    <div class="d-block d-md-flex d-sm-none">
+      <div class="d-inline">one</div>
+      <div class="d-inline">two</div>
+    </div>
+    <div class="d-print-none">print hidden</div>
+
+    <v-row>
+      <v-col v-for="n in 25" :key="n" cols="auto">
+        <v-card :elevation="n - 1" height="100" width="100">
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+            v-text="n - 1"></v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-slider
+          v-model="selected"
+          prepend-icon="mdi-pan-horizontal"
+          min="0"
+          max="24"
+          thumb-label=""></v-slider>
+      </v-col>
+    </v-row>
+    <div class="text--primary">
+      <v-hover>
+        <template v-slot="{ hover }">
+          <v-card
+            :elevation="hover ? 24 : 6"
+            class="mx-auto pa-6">
+            Prop base elevation
+          </v-card>
+        </template>
+      </v-hover>
+      <div class="my-6"></div>
+      <v-hover>
+        <template v-slot="{ hover }">
+          <v-card
+            :class="`elevation-${hover ? 24 : 6}`"
+            class="mx-auto pa-6 transition-swing">
+            class base elevation
+          </v-card>
+        </template>
+      </v-hover>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data () {
+    return {
+      selected: 0
+    }
   }
 }
 </script>
